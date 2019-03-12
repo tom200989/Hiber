554d4c13fbb84ef5ede4fca51c583aa3b137963d
qianli.ma <qianli.ma@jrdcom.com>
2019-03-12 14:07:06 +0800

    增加AES加密

--- app/src/main/java/com/hiber/tools/RootEncrypt.java
+++ app/src/main/java/com/hiber/tools/RootEncrypt.java
@@ -4,6 +4,8 @@ package com.hiber.tools;
  */
 
 
+import com.hiber.cons.Cons;
+import com.hiber.tools.encoder.Aes128;
 import com.hiber.tools.encoder.Des;
 
 public class RootEncrypt {
@@ -19,7 +21,7 @@ public class RootEncrypt {
             return Des.encrypt(mingwen, "roothiber");
         } catch (Exception e) {
             e.printStackTrace();
-            Lgg.t(Lgg.TAG).vv("encrypt error: " + e.getMessage());
+            Lgg.t(Cons.TAG).vv("Des-encrypt error: " + e.getMessage());
         }
         return mingwen;
     }
@@ -35,8 +37,80 @@ public class RootEncrypt {
             return Des.decrypt(miwen, "roothiber");
         } catch (Exception e) {
             e.printStackTrace();
-            Lgg.t(Lgg.TAG).vv("descrypt error: " + e.getMessage());
+            Lgg.t(Cons.TAG).vv("Des-descrypt error: " + e.getMessage());
         }
         return miwen;
+    }
+
+    /**
+     * AES加密(CBC)
+     *
+     * @param ori 原文
+     * @param key 密钥
+     * @param iv  向量
+     * @return 密文
+     */
+    public static String aes_encrypt_CBC(String ori, byte[] key, byte[] iv) {
+        try {
+            return Aes128.encrypt(ori, key, iv, Aes128.Transformation_CBC);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Lgg.t(Cons.TAG).vv("Aes-encrypt CBC error: " + e.getMessage());
+        }
+        return "";
+    }
+
+    /**
+     * AES加密(CFB)
+     *
+     * @param ori 原文
+     * @param key 密钥
+     * @param iv  向量
+     * @return 密文
+     */
+    public static String aes_encrypt_CFB(String ori, byte[] key, byte[] iv) {
+        try {
+            return Aes128.encrypt(ori, key, iv, Aes128.Transformation_CFB);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Lgg.t(Lgg.TAG).vv("Aes-encrypt CFB error: " + e.getMessage());
+        }
+        return "";
+    }
+
+    /**
+     * AES解密(CBC)
+     *
+     * @param ori 密文
+     * @param key 密钥
+     * @param iv  向量
+     * @return 原文
+     */
+    public static String aes_decrypt_CBC(String ori, byte[] key, byte[] iv) {
+        try {
+            return Aes128.decrypt(ori, key, iv, Aes128.Transformation_CBC);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Lgg.t(Cons.TAG).vv("Aes-descrypt error: " + e.getMessage());
+        }
+        return "";
+    }
+    
+    /**
+     * AES解密(CFB)
+     *
+     * @param ori 密文
+     * @param key 密钥
+     * @param iv  向量
+     * @return 原文
+     */
+    public static String aes_decrypt_CFB(String ori, byte[] key, byte[] iv) {
+        try {
+            return Aes128.decrypt(ori, key, iv, Aes128.Transformation_CFB);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Lgg.t(Cons.TAG).vv("Aes-descrypt error: " + e.getMessage());
+        }
+        return "";
     }
 }
