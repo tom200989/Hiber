941536f864e4fd2221c4aca2760c0dfbe22138b7
qianlima <qianlima@tcl.com>
2019-08-22 10:24:21 +0800

    修复切换语言的bug

--- app/src/main/java/com/hiber/hiber/language/LangHelper.java
+++ app/src/main/java/com/hiber/hiber/language/LangHelper.java
@@ -94,7 +94,7 @@ public class LangHelper {
         }
         DisplayMetrics dm = resources.getDisplayMetrics();
         resources.updateConfiguration(configuration, dm);
-        Locale.setDefault(locale);
+        Locale.setDefault(locale);// 该句必须要重新刷新
     }
 
     /**
